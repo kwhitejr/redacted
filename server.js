b5bb6579afaf1a11e26a5b5482992dac83308ae2
kwhitejr <kwhitejr.edu@gmail.com>
2016-01-06 21:28:44 -1000

    post body parser working

--- server.js
+++ server.js
@@ -1,6 +1,40 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
+
+app.use(bodyParser.urlencoded({extend: false}));
+
+app.use(function (req, res, next) {
+  console.log('The parsed body is: ');
+  console.log(req.body);
+  return next();
+});
+
+app.route('/')
+  .get(function (req, res) {
+
+  })
+  .post(function (req, res) {
+
+  });
+
+function messageConverter(msg) {
+  var censorship = [
+    {'selfie': 'self-portrait'},
+    {'yummers': 'delicious'},
+    {'outchea': 'are out here'},
+    {'bruh': 'wow'},
+    {'doge': 'pug'},
+    {'cilantro': 'soap'},
+    {'bae': 'loved one'},
+    {'swag': 'style'},
+    {'yolo': 'carpe diem'}
+  ];
+  censorship.forEach(function (pair) {
+    msg.replace(pair, pair[key]);
+  });
+}
 
 var server = app.listen(3000, function () {
   var port = server.address().port;
