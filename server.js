55f6dbe689dd80ddfe7982651440b244b9a95195
kwhitejr <kwhitejr.edu@gmail.com>
2016-01-07 18:39:22 -1000

    converter function working; implemented res.locals variables

--- server.js
+++ server.js
@@ -11,29 +11,49 @@ app.use(function (req, res, next) {
   return next();
 });
 
+app.use('/',
+  validateMessage,
+  messageConverter
+);
+
 app.route('/')
   .get(function (req, res) {
-
+    console.log('rendering index.jade...');
+    res.render('index', {pageTitle: msgConverter});
   })
   .post(function (req, res) {
-
+    res.send(res.locals.msg);
   });
 
-function messageConverter(msg) {
-  var censorship = [
-    {'selfie': 'self-portrait'},
-    {'yummers': 'delicious'},
-    {'outchea': 'are out here'},
-    {'bruh': 'wow'},
-    {'doge': 'pug'},
-    {'cilantro': 'soap'},
-    {'bae': 'loved one'},
-    {'swag': 'style'},
-    {'yolo': 'carpe diem'}
-  ];
-  censorship.forEach(function (pair) {
-    msg.replace(pair, pair[key]);
-  });
+function validateMessage(req, res, next) {
+  if (!req.body.msg) {
+    res.status(400);
+    // sending both writes and ends the connection/stream
+    return res.send('Please provide a \'msg\'');
+  }
+  res.locals.msg = req.body.msg;
+  return next();
+}
+
+function messageConverter(req, res, next) {
+  var censorship = {
+    'selfie': 'self-portrait',
+    'yummers': 'delicious',
+    'outchea': 'are out here',
+    'bruh': 'wow',
+    'doge': 'pug',
+    'cilantro': 'soap',
+    'bae': 'loved one',
+    'swag': 'style',
+    'yolo': 'carpe diem'
+  };
+  for (var word in censorship) {
+    // Modify this code to refine the search-and-replace functionality.
+    var oldWord = new RegExp(word, 'gi');
+    res.locals.msg = res.locals.msg.replace(oldWord, censorship[word]);
+  }
+  console.log('this is res locals: ', res.locals);
+  return next();
 }
 
 var server = app.listen(3000, function () {
